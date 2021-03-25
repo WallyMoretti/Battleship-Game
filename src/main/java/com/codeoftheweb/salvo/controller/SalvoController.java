f3f07385f4eaf1e7a7c0ce4a9cccd34d531dc1d0
wally <wally.moretti@gmail.com>
2021-03-25 11:48:01 -0300

    Modularizing /game_view/{gamePlayerId}

--- src/main/java/com/codeoftheweb/salvo/controller/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/controller/SalvoController.java
@@ -1,17 +1,22 @@
 package com.codeoftheweb.salvo.controller;
 
+import com.codeoftheweb.salvo.models.GamePlayer;
 import com.codeoftheweb.salvo.repository.GamePlayerRepository;
 import com.codeoftheweb.salvo.repository.GameRepository;
 import com.codeoftheweb.salvo.repository.PlayerRepository;
 import com.codeoftheweb.salvo.repository.ShipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController //  Controlador para construir una API Rest que reciba y envíe peticiones requeridas.
@@ -30,6 +35,8 @@ public class SalvoController {
     @Autowired
     private ShipRepository shipRepository;
 
+
+    // -- Metodos -- //
     @GetMapping("/games") // Lo mismo que @RequestMapping, pero solo a nivel método.
     public List<Object> getGames() {
         return gameRepository.findAll().stream().map(game -> game.makeGameDTO()).collect(Collectors.toList());
@@ -45,14 +52,32 @@ public class SalvoController {
         return playerRepository.findAll().stream().map(player -> player.makePlayerDTO()).collect(Collectors.toList());
     }
 
-    @GetMapping("/game_view/{gamePlayerID}")
-    public Map<String, Object> getGameView(@PathVariable Long gamePlayerID) { // @PathVariable obtiene el valor específico de una URL.
+    @GetMapping("/game_view/{gamePlayerId}")
+    public ResponseEntity<Map<String, Object>> findGamePlayer(@PathVariable Long gamePlayerId) {
 
-        // Desde gamePlayerRepository, accedo a Game para utilizar su DTO.
-        Map<String, Object> data = gamePlayerRepository.getOne(gamePlayerID).getGame().makeGameDTO();
+        Optional<GamePlayer> gp = gamePlayerRepository.findById(gamePlayerId);
+        ResponseEntity<Map<String, Object>> response;
 
-        // Agrego al Map 'data' los ships, accediendo desde gamePlayerRepository, para que 'data' guarde el retorno del DTO de la clase Ship.
-        data.put("ships", gamePlayerRepository.getOne(gamePlayerID).getShips().stream().map(ship -> ship.makeShipDTO()).collect(Collectors.toList()));
+        if (gp.isPresent()) { // Si no es null, realizo un llamado al metodo "getMapDTOs" que devuelve un mapa con los DTO.
+
+            response = new ResponseEntity<>(getMapDTOs(gamePlayerId), HttpStatus.OK);
+        } else {
+
+            Map<String, Object> aux = new LinkedHashMap<String, Object>();
+            aux.put("Problem", "gamePlayer does not exist");
+            response = new ResponseEntity<>(aux, HttpStatus.UNAUTHORIZED);
+        }
+        return response;
+    }
+
+    private Map<String, Object> getMapDTOs(Long gamePlayerId) { // Retorna un mapa con los DTOs de las clases.
+
+        // Creo un Mapa donde agrego los DTO, accediendo desde gamePlayerRepository.
+        // Accedo a la clase Game, que posee 'makeGameDTO' donde este tiene anidados los DTO de GamePlayer y Player respectivamente.
+        Map<String, Object> data = gamePlayerRepository.getOne(gamePlayerId).getGame().makeGameDTO();
+
+        // Agrego a 'data' el DTO de Ship.
+        data.put("ships", gamePlayerRepository.getOne(gamePlayerId).getShips().stream().map(ship -> ship.makeShipDTO()).collect(Collectors.toList()));
 
         return data;
     }
