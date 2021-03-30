1d89a040f39d989509e6bcee277f747e23cc424a
wally <wally.moretti@gmail.com>
2021-03-30 17:47:43 -0300

    Changing variable names

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -11,15 +11,15 @@ function loadData() {
   $.get('/api/game_view/' + getParameterByName('gp'))
     .done(function (data) {
       var playerInfo;
-      if (data.gamePlayer[0].id == getParameterByName('gp'))
-        playerInfo = [data.gamePlayer[0].player, data.gamePlayer[1].player];
+      if (data.gamePlayers[0].id == getParameterByName('gp'))
+        playerInfo = [data.gamePlayers[0].player, data.gamePlayers[1].player];
       else
-        playerInfo = [data.gamePlayer[1].player, data.gamePlayer[0].player];
+        playerInfo = [data.gamePlayers[1].player, data.gamePlayers[0].player];
 
-      $('#playerInfo').text(playerInfo[0].username + '(you) vs ' + playerInfo[1].username);
+      $('#playerInfo').text(playerInfo[0].email + '(you) vs ' + playerInfo[1].email);
 
       data.ships.forEach(function (shipPiece) {
-        shipPiece.location.forEach(function (shipLocation) {
+        shipPiece.locations.forEach(function (shipLocation) {
           if(isHit(shipLocation,data.salvoes,playerInfo[0].id)  !=  0){
             $('#B_' + shipLocation).addClass('ship-piece-hited');
             $('#B_' + shipLocation).text(isHit(shipLocation,data.salvoes,playerInfo[0].id));
@@ -31,13 +31,13 @@ function loadData() {
       });
       data.salvoes.forEach(function (salvo) {
         if (playerInfo[0].id === salvo.player) {
-          salvo.location.forEach(function (location) {
+          salvo.locations.forEach(function (location) {
             $('#S_' + location).addClass('salvo-piece');
             $('#S_' + location).text(salvo.turn);
 
           });
         } else {
-          salvo.location.forEach(function (location) {
+          salvo.locations.forEach(function (location) {
             $('#B_' + location).addClass('salvo');
           });
         }
@@ -52,7 +52,7 @@ function isHit(shipLocation,salvoes,playerId) {
   var turn = 0;
   salvoes.forEach(function (salvo) {
     if(salvo.player != playerId)
-      salvo.location.forEach(function (location) {
+      salvo.locations.forEach(function (location) {
         if(shipLocation === location)
           turn = salvo.turn;
       });
