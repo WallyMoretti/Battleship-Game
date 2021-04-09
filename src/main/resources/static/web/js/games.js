4295914c5f883bdffcbd4816361fcfd995b17a5a
wally <wally.moretti@gmail.com>
2021-04-09 12:51:16 -0300

    Changed 'email' to 'username' in line 45

--- src/main/resources/static/web/js/games.js
+++ src/main/resources/static/web/js/games.js
@@ -42,7 +42,7 @@ $('#login-form').on('submit', function (event) {
 
     } else if (submitButton == "signup") {
         $.post("/api/players",
-            { email: $("#username").val(),
+            { username: $("#username").val(),
                 password: $("#password").val() })
             .done(function(data) {
                 console.log("signup ok");
